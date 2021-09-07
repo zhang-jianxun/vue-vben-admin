a0fa57a9cbc73158a75eb15a63b287002bc62a27
zhangjianxun <zhangjianxun@ts-it.cn>
2021-09-07 17:06:29 +0800

    perf: 全局按需注册组件

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -5,6 +5,39 @@ import {
   Button as AntButton,
   Input,
   Layout,
+  Select,
+  Alert,
+  Checkbox,
+  DatePicker,
+  Radio,
+  Switch,
+  Card,
+  List,
+  Tabs,
+  Descriptions,
+  Tree,
+  Table,
+  Divider,
+  Modal,
+  Drawer,
+  Dropdown,
+  Tag,
+  Tooltip,
+  Badge,
+  Popover,
+  Upload,
+  Transfer,
+  Steps,
+  PageHeader,
+  Result,
+  Empty,
+  Avatar,
+  Menu,
+  Breadcrumb,
+  Form,
+  Row,
+  Col,
+  Spin,
 } from 'ant-design-vue';
 
 const compList = [AntButton.Group];
@@ -14,5 +47,41 @@ export function registerGlobComp(app: App) {
     app.component(comp.name || comp.displayName, comp);
   });
 
-  app.use(Input).use(Button).use(Layout);
+  app
+    .use(Input)
+    .use(Button)
+    .use(Layout)
+    .use(Select)
+    .use(Alert)
+    .use(Breadcrumb)
+    .use(Checkbox)
+    .use(DatePicker)
+    .use(Radio)
+    .use(Switch)
+    .use(Card)
+    .use(List)
+    .use(Descriptions)
+    .use(Tree)
+    .use(Table)
+    .use(Divider)
+    .use(Modal)
+    .use(Drawer)
+    .use(Dropdown)
+    .use(Tag)
+    .use(Tooltip)
+    .use(Badge)
+    .use(Popover)
+    .use(Upload)
+    .use(Transfer)
+    .use(Steps)
+    .use(PageHeader)
+    .use(Result)
+    .use(Empty)
+    .use(Avatar)
+    .use(Menu)
+    .use(Tabs)
+    .use(Form)
+    .use(Row)
+    .use(Col)
+    .use(Spin);
 }
