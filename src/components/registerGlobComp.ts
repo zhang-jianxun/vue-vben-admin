e3ec2119dc62a5da222663d2ac887116684bed11
张建勋 <zhangjianxun@ts-it.cn>
2021-09-10 11:21:33 +0800

    perf: 配置自定义loading指示符

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -39,6 +39,18 @@ import {
   Col,
   Spin,
 } from 'ant-design-vue';
+import { h } from 'vue';
+import { LoadingOutlined } from '@ant-design/icons-vue';
+// 全局配置自定义loading指示符
+// Spin.setDefaultIndicator(<PageLoading />);
+Spin.setDefaultIndicator({
+  indicator: h(LoadingOutlined, {
+    style: {
+      fontSize: '24px',
+    },
+    spin: true,
+  }),
+});
 
 const compList = [AntButton.Group];
 
